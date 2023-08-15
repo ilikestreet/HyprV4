e9988a6f1d9a8d81336ff7ef65bc50985b18a35f
ilikestreet <ilikestreet.jz@gmail.com>
2023-08-16 00:39:35 +0800

    fix on release vm

--- win11-gaming/release/end/stop.sh
+++ win11-gaming/release/end/stop.sh
@@ -11,18 +11,20 @@ virsh nodedev-reattach pci_0000_0c_00_1
 #modprobe -r vfio
 #modprobe -r vfio_iommu_type1
 modprobe -r vfio_pci
+
 while [ 1 ];
 do
   qemu_pid=$(ps aux | grep qemu-system | grep -v grep)
-  sleep 1
+  sleep 5
   if [ "$qemu_pid" == "" ] ;
   then
     break
   fi
 done
+
 # Rebind VT consoles
 echo 1 > /sys/class/vtconsole/vtcon0/bind
-echo 1 > /sys/class/vtconsole/vtcon1/bind
+#echo 1 > /sys/class/vtconsole/vtcon1/bind
 
 
 echo "loading nvidia mod"
@@ -33,12 +35,12 @@ modprobe nvidia
 echo "loaded nvidia mod"
 
 
-nvidia-xconfig --query-gpu-info > /dev/null 2>&1
+#nvidia-xconfig --query-gpu-info > /dev/null 2>&1
 #echo "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/bind
 
 echo "begin start display manager"
 
 # Restart Display Manager
-systemctl start sddm.service
+#systemctl start sddm.service
 systemctl start display-manager.service
 
